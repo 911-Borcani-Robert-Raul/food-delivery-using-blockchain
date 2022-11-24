5994625c71a11dac45ddf21b4623333c2feb9028
Borcani <robertraulborcani@gmail.com>
2022-11-24 14:16:10 +0200

    Created item

---
+++ contracts/domain/item.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.6.6;
+
+struct Item{
+    uint256 id;
+    string name;
+    string description;
+    uint256 price;
+}
--- contracts/food_delivery.sol
+++ contracts/food_delivery.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.6.6;
+import "./domain/item.sol";
 
 contract FoodDelivery {
     uint256 public test;
+    Item[] public items;
 
     constructor() public {
         test = 23;
