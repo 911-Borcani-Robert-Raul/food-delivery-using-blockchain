8ebfd78ae89e8169c237e9d4880d951550b1d527
Borcani <robertraulborcani@gmail.com>
2022-12-19 11:15:09 +0200

    Added clients, orders and some basic functionalities for them

--- contracts/food_delivery.sol
+++ contracts/food_delivery.sol
@@ -15,16 +15,27 @@ contract FoodDelivery {
         Item[] items;
     }
 
-    // function createRestaurant(address addr, string calldata name, string calldata description) private pure returns(Restaurant storage) {
-    //     Restaurant storage restaurant = ;
-    //     restaurant.addr = addr;
-    //     restaurant.name = name;
-    //     restaurant.description = description;
+    struct Client {
+        address addr;
+        string name;
+    }
 
-    //     return restaurant;
-    // }
+    struct Order {
+        uint256 id;
+        address restaurantAddr;
+        address clientAddr;
+        uint256[] itemIndices;
+        uint256[] quantities;
+        uint256 totalPrice;
+        bool isComplete;
+        bool isDelivered;
+    }
+
 
     mapping(address => Restaurant) public restaurants;
+    mapping(uint256 => Order) public orders;
+    mapping(address => Client) public clients;
+    uint256 numberOfOrders = 0;
 
     function registerRestaurant(string calldata name, string calldata description) public {
         Restaurant storage restaurant = restaurants[msg.sender];
@@ -32,5 +43,46 @@ contract FoodDelivery {
         restaurant.name = name;
         restaurant.description = description;
         restaurants[msg.sender] = restaurant;
+    }
+
+    function registerClient(string calldata name) public {
+        Client storage client = clients[msg.sender];
+        client.addr = msg.sender;
+        client.name = name;
+        clients[msg.sender] = client;
+    }
+
+    function addItem(uint256 id, string calldata name, string calldata description, uint256 price) public {
+        Restaurant storage restaurant = restaurants[msg.sender];
+        restaurant.items.push(Item({
+            id: id,
+            name: name,
+            description: description,
+            price: price
+        }));
+        restaurants[msg.sender] = restaurant;
+    }
+
+    function placeOrder(address restaurantAddr, uint256[] memory itemIds, uint256[] memory quantities) public {
+        Restaurant storage restaurant = restaurants[restaurantAddr];
+        uint256 totalPrice = 0;
+
+        for (uint256 i = 0; i < itemIds.length; i++) {
+            Item memory item = restaurant.items[itemIds[i]];
+            totalPrice += item.price * quantities[i];
+        }
+
+        uint256 id = numberOfOrders++;
+
+        orders[id] = Order({
+            id: id,
+            restaurantAddr: restaurantAddr,
+            clientAddr: msg.sender,
+            itemIndices: itemIds,
+            quantities: quantities,
+            totalPrice: totalPrice,
+            isComplete: false,
+            isDelivered: false
+        });
     }
 }
